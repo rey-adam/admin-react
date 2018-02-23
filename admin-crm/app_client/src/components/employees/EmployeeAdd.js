cc70ada5eee8f31b9c09b2cadfe2607c84a6bea7
Rey Cruz <reycruz@dhcp-v076-146.mobile.uci.edu>
2018-02-22 21:02:00 -0800

    employee can be added

--- admin-crm/app_client/src/components/employees/EmployeeAdd.js
+++ admin-crm/app_client/src/components/employees/EmployeeAdd.js
@@ -14,40 +14,43 @@ class EmployeeAdd extends React.Component {
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
+
     handleChange(event) {
-      this.setState({firstName: event.target.firstName});
-      this.setState({lasttName: event.target.lastName});
-      this.setState({jobTitle: event.target.jobTitle});
-      this.setState({age: event.target.age});
+      const {name, value } = event.target;
+      this.setState({ [name]: value});
     }
   
     handleSubmit(event) {
-      alert('A new employee was submitted: ' + this.state.firstName + this.state.lastName + this.state.jobTitle + this.state.age);
+      alert('A new employee was submitted: ' + this.state.firstName + " " + this.state.lastName + " "  + this.state.jobTitle + " "  + this.state.age);
       event.preventDefault();
     }
   
+
     render() {
       return (
+        <div class="row">
+        <div class="col-md-4 col-centered">
         <form onSubmit={this.handleSubmit}>
           <label>
            First Name:
-            <input type="text" value={this.state.firstName} onChange={this.handleChange} />
+            <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
           </label>
           <label>
             Last Name:
-            <input type="text" value={this.state.lastName} onChange={this.handleChange} />
+            <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
           </label>
           <label>
             Job Title:
-            <input type="text" value={this.state.jobTitle} onChange={this.handleChange} />
+            <input type="text" name="jobTitle" value={this.state.jobTitle} onChange={this.handleChange}/>
           </label>
           <label>
             Age:
-            <input type="number" value={this.state.age} onChange={this.handleChange} />
+            <input type="number" name="age" value={this.state.age} onChange={this.handleChange} />
           </label>
           <input type="submit" value="Submit" />
         </form>
+        </div>
+        </div>
       );
     }
   }
